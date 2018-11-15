0ffa1f1195ef48b7c856e08e9c9c7fbb1329bd67
Green Man <brendan.mccaughey@gmail.com>
2018-11-14 21:59:53 -0500

    Sunset image effect completed. It doesnt seem to be playing nice with the rest of the page, however.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,29 +9,29 @@
 //     .setPin("#sky") // pins the element for the the scene's duration
 //     .addTo(controller); // assign the scene to the controller
 
-    // var regex = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/,
-    // sky = document.getElementById("sky"),
-    // mountainLayers = sky.querySelectorAll("path"),
-    // SVGoffsettop = sky.getBoundingClientRect().top,
-    // vertHeight = sky.getBoundingClientRect().height,
-    // sun = document.getElementById("sun");
+    var regex = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/,
+    sky = document.getElementById("sky"),
+    mountainLayers = sky.querySelectorAll("path"),
+    SVGoffsettop = sky.getBoundingClientRect().top,
+    vertHeight = sky.getBoundingClientRect().height,
+    sun = document.getElementById("sun");
     
-    // function scrollHandler() {
-    //       if (window.scrollY < vertHeight) {
-    //       Array.prototype.forEach.call(mountainLayers, function(layer) { 
-    //           var layerFill = layer.getAttribute("fill"),
-    //           vertRoll = Math.abs(window.scrollY - vertHeight) / vertHeight;
-    //           hslComponents = layerFill.match(regex).slice(1),
-    //           newHSL = parseFloat(hslComponents[2]) * vertRoll;
-    //           layer.style.fill = "hsl(" + hslComponents[0] +", "+ hslComponents[1] + "%, " +  newHSL + "%)";
-    //           sky.style.background = "hsl(48, " + 100 * vertRoll + "%, " + 88 * vertRoll + "%)";
-    //           sun.style.transform = "translate3d(0," + window.scrollY / 10 + "px, 0)";
-    //         })
-    //         } else {
-    //             sky.style.transform = "translateY(-"+ (window.scrollY - vertHeight)+"px)";
-    //     }
-    // }
+    function scrollHandler() {
+          if (window.scrollY < vertHeight) {
+          Array.prototype.forEach.call(mountainLayers, function(layer) { 
+              var layerFill = layer.getAttribute("fill"),
+              vertRoll = Math.abs(window.scrollY - vertHeight) / vertHeight;
+              hslComponents = layerFill.match(regex).slice(1), 
+              newHSL = parseFloat(hslComponents[2]) * vertRoll;
+              layer.style.fill = "hsl(" + hslComponents[0] +", "+ hslComponents[1] + "%, " +  newHSL + "%)";
+              sky.style.background = "linear-gradient(hsl(320, " + 100 * vertRoll + "%, " + 88 * vertRoll + "%),hsl(21, " + 100 * vertRoll + "%, " + 88 * vertRoll + "%),hsl(46, " + 100 * vertRoll + "%, " + 88 * vertRoll + "%))";
+              sun.style.transform = "translate3d(0," + window.scrollY / 10 + "px, 0)";
+            })
+            } else {
+                sky.style.transform = "translateY(-"+ (window.scrollY - vertHeight)+"px)";
+        }
+    }
     
-    // window.onscroll = function() {
-    //         window.requestAnimationFrame(scrollHandler);
-    // }
+    window.onscroll = function() {
+            window.requestAnimationFrame(scrollHandler);
+    }
